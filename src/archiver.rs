2650d0f4c143afcaf278095bf201628e3f5f27d0
vicanso <tree.xie@outlook.com>
2024-02-28 19:22:05 +0800

    refactor: adjust glob pattern

--- src/archiver.rs
+++ src/archiver.rs
@@ -1,6 +1,6 @@
 use glob::glob;
 use std::path::Path;
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::SystemTime;
 use tokio::fs::File;
 use tokio_tar::Builder;
 use tracing::info;
@@ -17,10 +17,7 @@ const SNAPPY: &str = "sz";
 const DEFLATE: &str = "zip";
 
 fn uuid() -> String {
-    let d = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap_or_default();
-    let ts = Timestamp::from_unix(NoContext, d.as_secs(), d.subsec_nanos());
+    let ts = Timestamp::now(NoContext);
     Uuid::new_v7(ts).to_string()
 }
 
@@ -46,44 +43,52 @@ pub async fn archive(original: &str, target: &str, level: i32) -> Result<(), Err
     let mut file_count = 0;
     let start = SystemTime::now();
 
-    for suffix in ["/*", "/*/*", "/**/*"] {
-        for entry in
-            glob(&(original.to_string() + suffix)).map_err(|err| Error::Pattern { source: err })?
-        {
-            let file_path = entry.map_err(|err| Error::Glob { source: err })?;
-            let filename = file_path
-                .strip_prefix(original)
-                .map_err(|err| Error::StripPrefix { source: err })?;
-            if file_path.is_dir() {
-                continue;
-            }
+    for entry in
+        glob(&(original.to_string() + "/**/*")).map_err(|err| Error::Pattern { source: err })?
+    {
+        let file_path = entry.map_err(|err| Error::Glob { source: err })?;
+        let filename = file_path
+            .strip_prefix(original)
+            .map_err(|err| Error::StripPrefix { source: err })?;
+        if file_path.is_dir() {
+            continue;
+        }
 
-            let file = dir.path().join(uuid());
+        let file = dir.path().join(uuid());
 
-            match compress_type {
-                GZIP => compression::gzip_encode(&file_path, &file, level).await,
-                ZSTD => compression::zstd_encode(&file_path, &file, level).await,
-                BROTLI => compression::brotli_encode(&file_path, &file, level).await,
-                SNAPPY => compression::snappy_encode(&file_path, &file).await,
-                LZ4 => compression::lz4_encode(&file_path, &file).await,
-                DEFLATE => compression::deflate_encode(&file_path, &file, level).await,
-                _ => Err(Error::InvalidCompression {
-                    compression: compress_type.to_string(),
-                }),
-            }?;
-            a.append_file(filename, &mut File::open(&file).await?)
-                .await?;
-            file_count += 1;
+        match compress_type {
+            GZIP => compression::gzip_encode(&file_path, &file, level).await,
+            ZSTD => compression::zstd_encode(&file_path, &file, level).await,
+            BROTLI => compression::brotli_encode(&file_path, &file, level).await,
+            SNAPPY => compression::snappy_encode(&file_path, &file).await,
+            LZ4 => compression::lz4_encode(&file_path, &file).await,
+            DEFLATE => compression::deflate_encode(&file_path, &file, level).await,
+            _ => Err(Error::InvalidCompression {
+                compression: compress_type.to_string(),
+            }),
+        }?;
+        a.append_file(filename, &mut File::open(&file).await?)
+            .await?;
+        file_count += 1;
+    }
+    a.finish().await?;
+    let mut duration = None;
+    if let Ok(d) = SystemTime::now().duration_since(start) {
+        duration = Some(humantime::format_duration(d).to_string());
+    };
+    let mut file_size = None;
+    if let Ok(file) = File::open(target).await {
+        if let Ok(meta) = file.metadata().await {
+            file_size = Some(bytesize::ByteSize(meta.len()).to_string());
         }
     }
-    let duration = if let Ok(d) = SystemTime::now().duration_since(start) {
-        humantime::format_duration(d).to_string()
-    } else {
-        "".to_string()
-    };
     info!(
+        file = target,
+        file_size,
         compression = compress_type,
-        level, file_count, duration, "archive done",
+        level,
+        file_count,
+        duration,
     );
 
     Ok(())
